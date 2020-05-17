7a755129bdf5a2aca4fe50a821df380fd13bd00b
magicdawn <magicdawn@qq.com>
2020-05-17 17:47:06 +0800

    test: add test code

---
+++ test/compress.test.js
@@ -0,0 +1,20 @@
+const {mozjpegCompress} = require('../lib/compress')
+const {metadata} = require('../lib/codec/decode')
+
+describe('compress', () => {
+  const input = __dirname + '/fixtures/demo.png'
+
+  it('mozjpegCompress ', async () => {
+    const buf = await mozjpegCompress(input)
+
+    // see what's in the compress result
+    const meta = await metadata(buf)
+    const {format, width, height} = meta
+
+    format.should.equal('jpeg')
+
+    // iPhone 7
+    width.should.equal(750)
+    height.should.equal(1334)
+  })
+})
---
+++ test/decode.test.js
@@ -0,0 +1,28 @@
+const {metadata, decode} = require('../lib/codec/decode')
+
+describe('decode', () => {
+  const input = __dirname + '/fixtures/demo.png'
+
+  it('metadata works', async () => {
+    const meta = await metadata(input)
+    const {format, width, height} = meta
+
+    format.should.equal('png')
+
+    // iPhone 7
+    width.should.equal(750)
+    height.should.equal(1334)
+  })
+
+  it('decode', async () => {
+    const decoded = await decode(input)
+    const {data, width, height} = decoded
+
+    data.should.instanceof(Uint8Array)
+    data.byteLength.should.above(0)
+
+    // iPhone 7
+    width.should.equal(750)
+    height.should.equal(1334)
+  })
+})
